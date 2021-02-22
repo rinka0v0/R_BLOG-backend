f6fd0f25492804de7628aafa8b11e1523acca81f
rinka0v0 <rinka-layla.love2.0409@docomo.ne.jp>
2021-02-22 18:04:24 +0900

    'add jwt api and signup api'

--- src/routes/samples.js
+++ src/routes/samples.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* サンプルAPI① 
  * http://localhost:3000/samples にGETメソッドのリクエストを投げると、
  * JSON形式で文字列を返す。
  */
 router.get('/', function(req, res, next) {
-  var param = {"値":"これはサンプルAPIです!!!"};
+  const param = {"値":"これはサンプルAPIです!!!"};
   res.header('Content-Type', 'application/json; charset=utf-8')
   res.send(param);
 });
@@ -16,7 +16,7 @@ router.get('/', function(req, res, next) {
  * JSON形式で文字列を返す。
  */
 router.get('/hello', function(req, res, next) {
-  var param = {"result":"Hello World !"};
+  const param = {"result":"Hello World !"};
   res.header('Content-Type', 'application/json; charset=utf-8')
   res.send(param);
 });
@@ -26,7 +26,7 @@ router.get('/hello', function(req, res, next) {
  * JSON形式で(任意の文字列)を返す。
  */
 router.get('/hello/:place', function (req, res, next) {
-    var param = {"result":"Hello "+ req.params.place + " !"};      // レスポンスで返す値。JSON形式。
+    const param = {"result":"Hello "+ req.params.place + " !"};      // レスポンスで返す値。JSON形式。
     res.header('Content-Type', 'application/json; charset=utf-8')  // 「レスポンスはJSON形式で返すよ」の意味
     res.send(param);                                               // 「レスポンス送るよ」の意味
   });
